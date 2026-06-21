b0ed131e8c4add190bb21ae3f215cc29b8b0d9cb
Linbergh <vincelinberghlorenzo@gmail.com>
2026-06-21 08:59:02 +0800

    added computations on total price and discounted price if qualified

--- shopping-cart.py
+++ shopping-cart.py
@@ -9,6 +9,24 @@ products = [
     ("Rice", 2.10),
 ]
 
+cart = [products[0], products[2], products[6], products[3]]
 
 for index, product in enumerate(products, start=1):
     print(f"{index}. {product[0]} - ${product[1]:.2f}")
+
+
+print("--- Receipt ---")
+total = 0
+
+for items in cart:
+    print(f"{items[0]} - {items[1]}")
+    total += items[1]
+
+print("---------------")
+
+if total > 10:
+    print(f"Total: {total}")
+    print("You qualify for a 10% discount!")
+    print(f"Discounted Price: {total-(total*.1)}")
+else:
+    print(f"Total: {total}")
