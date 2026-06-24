b90fccc9e50bee33aabf2db970176dfe59821f97
Bins <vincelinberghlorenzo@gmail.com>
2026-06-24 21:30:43 +0800

    add validation for deposit  and withdrawal amounts

--- bank-account-system.py
+++ bank-account-system.py
@@ -1,48 +1,56 @@
+balance = 0
+transaction = []
+
+
 def deposit(amount):
-    global balance, transaction
+    global balance
+
+    if amount <= 0:
+        print("Deposit amount must be greater than zero!")
+        return
+
     balance += amount
-
     transaction.append((amount, "deposit"))
-
-    print(f"Deposited ${amount:.2f}")
+    print(f"Deposited ${amount:.2f} | Balance ${balance:.2f}")
 
 
 def withdraw(amount):
-    global balance, transaction
+    global balance
 
-    if balance < amount:
+    if amount < 0:
+        print("Withdrawal amount must be greater then zero!")
+        return
+    if amount > balance:
         print("Insufficient funds!")
-    else:
-        balance -= amount
+        return
 
-        transaction.append((amount, "withdrawal"))
-
-        print(f"Withdrawn ${amount:.2f}")
+    balance -= amount
+    transaction.append((amount, "withdrawal"))
+    print(f"Withdrawn ${amount:.2f} | Balance ${balance:.2f}")
 
 
 def get_balance():
-    global balance
-
-    print(f"\nRemaining balance: ${balance:.2f}")
+    print(f"\nCurrent balance: ${balance:.2f}")
 
 
 def transaction_history():
+    if not transaction:
+        print("No transactions yet!")
+        return
+
     print("\n--- Transaction History ---")
     for amount, type in transaction:
-        if type == "deposit":
-            print(f"+ ${amount:.2f} ({type})")
-        else:
-            print(f"- ${amount:.2f} ({type})")
+        symbol = "+" if type == "deposit" else "-"
+
+        print(f"{symbol} ${amount:.2f} ({type})")
     print("---------------------------")
-
-
-balance = 0
-transaction = []
 
 
 deposit(50)
 deposit(100)
 withdraw(30)
 withdraw(200)
+withdraw(-50)
+deposit(-20)
 transaction_history()
 get_balance()
