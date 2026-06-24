93e07f8a71df44711d61faa4d6204378d64ee76f
Bins <vincelinberghlorenzo@gmail.com>
2026-06-24 19:58:47 +0800

    testing functions with data

--- bank-account-system.py
+++ bank-account-system.py
@@ -1,18 +1,35 @@
-def deposit():
-    pass
+def deposit(amount):
+    global balance, transaction
+    balance += amount
+
+    transaction.append((amount, "deposit"))
 
 
-def withdraw():
-    pass
+def withdraw(amount):
+    global balance, transaction
+    balance -= amount
+
+    transaction.append((amount, "withdrawal"))
 
 
-def get_balance():
+def get_balance(amount):
     pass
 
 
 def transcation_history():
-    pass
+    print("--- Transaction History ---")
+    for amount, type in transaction:
+        if type == "Deposit":
+            print(f"+ ${amount:.2f} ({type})")
+        else:
+            print(f"- ${amount:.2f} ({type})")
+    print("---------------------------")
 
 
 balance = 0
 transaction = []
+
+
+deposit(50)
+withdraw(25)
+transcation_history()
