034579b2f1a5c52ac9cb1ae837a1be1cfbe07f97
Bins <vincelinberghlorenzo@gmail.com>
2026-06-26 20:29:43 +0800

    initial commit, added data to work with

---
+++ student-report-system.py
@@ -0,0 +1,8 @@
+students = [
+    {"name": "Alice", "grades": [92, 88, 95, 79, 84]},
+    {"name": "Bob", "grades": [55, 60, 48, 70, 63]},
+    {"name": "Charlie", "grades": [78, 82, 74, 88, 91]},
+    {"name": "Diana", "grades": [45, 50, 39, 60, 55]},
+    {"name": "Eve", "grades": [88, 92, 96, 85, 90]},
+    {"name": "Frank", "grades": [61, 58, 72, 65, 70]},
+]
