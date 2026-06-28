d685245a1b8acbdb862855849f79850530b81f3b
Bins <vincelinberghlorenzo@gmail.com>
2026-06-28 21:55:25 +0800

    added data to work with

---
+++ phase2/list-comprehension-workout.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+words = ["hello", "world", "python", "is", "fun", "list", "comprehension", "rocks"]
+
+students = [
+    {"name": "Alice", "grade": 92},
+    {"name": "Bob", "grade": 55},
+    {"name": "Charlie", "grade": 78},
+    {"name": "Diana", "grade": 45},
+    {"name": "Eve", "grade": 88},
+]
