c6747f652d2cc1190b83dae570e41a4c9bc93328
Bins <vincelinberghlorenzo@gmail.com>
2026-06-29 09:25:46 +0800

    helper function

---
+++ phase2/helper.py
@@ -0,0 +1,11 @@
+def letter_grade(grade):
+    if grade > 89:
+        return "A"
+    elif grade > 79:
+        return "B"
+    elif grade > 69:
+        return "C"
+    elif grade > 59:
+        return "D"
+    else:
+        return "F"
