1d2c7b22f74d5970dc30854ed2c2ce19ab8f872a
Bins <vincelinberghlorenzo@gmail.com>
2026-07-18 16:37:25 +0800

    warm up challenge after a  week of no coding due to work

---
+++ warmups/movie_list.py
@@ -0,0 +1,7 @@
+movies = [
+    {"title": "Inception", "year": 2010, "rating": 8.8},
+    {"title": "The Dark Knight", "year": 2008, "rating": 9.0},
+    {"title": "Interstellar", "year": 2014, "rating": 8.6},
+    {"title": "Parasite", "year": 2019, "rating": 8.5},
+    {"title": "The Godfather", "year": 1972, "rating": 9.2},
+]
