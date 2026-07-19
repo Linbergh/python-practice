79f51ce503909e5d460e2c142789b6edd424b882
Bins <vincelinberghlorenzo@gmail.com>
2026-07-19 22:29:34 +0800

    completed 1-6th task

--- phase2/bday_tracker/birthday_tracker.py
+++ phase2/bday_tracker/birthday_tracker.py
@@ -9,31 +9,93 @@ birthdays = [
 ]
 
 
-def proper_date(date):
-    return datetime.datetime.strptime(date, "%Y-%m-%d")
+def proper_date(date_str):
+    return datetime.datetime.strptime(date_str, "%Y-%m-%d").date()
 
 
 def get_age(bday):
     today = datetime.date.today()
     age = today.year - bday.year
 
-    if (today.month, today.year) < (bday.month, bday.year):
+    if (today.month, today.day) < (bday.month, bday.day):
         age -= 1
 
     return age
 
 
-def count_next_birthday(bday):
+def count_days(bday):
     today = datetime.date.today()
-    bday_count = today.month - bday.month
+    birthday = proper_date(bday)
 
-    return bday_count
+    next_birthday = birthday.replace(year=today.year)
+
+    if next_birthday < today:
+        next_birthday = next_birthday.replace(year=today.year + 1)
+
+    return (next_birthday - today).days
 
 
-bday = proper_date(birthdays[1]["birthday"])
-age = get_age(bday)
-next_bday = count_next_birthday(bday)
+def person_details(birthdays):
+    enriched = []
+
+    for birthday in birthdays:
+        age = get_age(proper_date(birthday["birthday"]))
+        n_days = count_days(birthday["birthday"])
+        bday = proper_date(birthday["birthday"])
+        enriched.append(
+            {
+                "name": birthday["name"],
+                "birthday": bday,
+                "age": age,
+                "days_until_bday": n_days,
+            }
+        )
+
+    return enriched
 
 
-print(next_bday)
-# print(bday)
+def print_section(header, birthdays):
+    print(f"--- {header} ---")
+    for bday in birthdays:
+        print(
+            f"{bday['name']}\n"
+            f" Birthday: {bday['birthday'].strftime('%B %d, %Y')}\n"
+            f" Age: {bday['age']} years old\n"
+            f" Days until next birthday: {bday['days_until_bday']}\n"
+        )
+
+
+def pritn_person(header, detail):
+    print(f"--- {header} ---")
+    print(
+        f"{detail['name']}\n"
+        f" Birthday: {detail['birthday'].strftime('%B %d, %Y')}\n"
+        f" Age: {detail['age']} years old\n"
+        f" Days until next birthday: {detail['days_until_bday']}\n"
+    )
+
+
+def get_nearest_bday(birthdays):
+    return min(birthdays, key=lambda bday: bday["days_until_bday"])
+
+
+def get_oldest(birthdays):
+    return max(birthdays, key=lambda bday: bday["age"])
+
+
+def get_youngest(birthdays):
+    return min(birthdays, key=lambda bday: bday["age"])
+
+
+def group_birthdays(birthdays):
+    pass
+
+
+enriched_birthdays = person_details(birthdays)
+
+
+# print_section("Personal Details", enriched_birthdays)
+# pritn_person("Nearest upcoming birthday", get_nearest_bday(enriched_birthdays))
+# pritn_person("Oldest", get_oldest(enriched_birthdays))
+# pritn_person("Youngest", get_youngest(enriched_birthdays))
+group_birthdays(enriched_birthdays)
